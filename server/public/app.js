247ce2148bb59c780df7321ba513506713388c48
Otavie <otavieokuoyo@gmail.com>
2024-04-06 04:32:34 +0100

    Add code activity display

--- server/public/app.js
+++ server/public/app.js
@@ -2,6 +2,7 @@ const socket = io('ws://localhost:3400')
 const input = document.querySelector('input')
 const form = document.querySelector('form')
 const ul = document.querySelector('ul')
+const activity = document.querySelector('.activity')
 
 function sendMessage(e) {
     e.preventDefault()
@@ -17,7 +18,24 @@ form.addEventListener('submit', sendMessage)
 
 // Listen for message we may receive from the server
 socket.on('message', (data) => {
+    activity.textContent = ''
     const li = document.createElement('li')
     li.textContent = data
     ul.appendChild(li)
+})
+
+input.addEventListener('keypress', () => {
+    socket.emit('activity', socket.id.substring(0, 5))
+})
+
+let activityTimer
+
+socket.on('activity', (username) => {
+    activity.textContent = `${username} typing...`
+
+    // Clear after 3 seconds without sending message
+    clearTimeout(activityTimer)
+    activityTimer = setTimeout(() => {
+        activity.textContent = ''
+    }, 1500)
 })
